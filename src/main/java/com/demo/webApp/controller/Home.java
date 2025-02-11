dd37cedc15b723f58e8a32b0c4b31449dde11878
Tejendra Kumar <tejendra@tej.local>
2025-02-11 12:46:51 +0100

    blank spring boot project

---
+++ src/main/java/com/demo/webApp/controller/Home.java
@@ -0,0 +1,16 @@
+package com.demo.webApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World!";
+    }
+
+}
